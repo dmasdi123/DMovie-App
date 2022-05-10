1cf6348d30411f09855d63ec9d8e1fbdb3a0cbab
dmasdi123 <febriandimas634@gmail.com>
2022-05-10 16:38:02 +0700

    add modal

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 $.ajax({
     url: 'http://www.omdbapi.com/?apikey=51559b83&s=superman',
-    success: function(result) {
-        console.log(result);
+    success: result => {
+        const movies = result.Search;
+        let cards = '';
+        movies.forEach( m => {
+            cards += `<div class="col-md-4 my-3">
+                            <div class="card">
+                                <img src="${m.Poster}">
+                                <div class="card-body">
+                                <h5 class="card-title">${m.Title}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                                <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#movieDetailModal">Show Details</a>
+                                </div>
+                            </div>
+                       </div>`;
+        });
+        $('.movie-container').html(cards);
+    },
+    error: function(e) {
+        console.log(e.reponseText);
     }
 });
